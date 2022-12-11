3bcc1342521f0f4cf5da2cff1c562e6c34b4bd87
elidianaandrade <lide_andrade_redes@hotmail.com>
2022-12-11 10:02:32 -0300

    setup

---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import  { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    :root {
+        --gray-600: #22272e;
+        --white: #FFFFFF;
+    }
+
+    *, body{
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        background-color: var(--gray-600);
+        color: var(--white);
+    }
+`
